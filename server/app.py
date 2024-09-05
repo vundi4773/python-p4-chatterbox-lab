2c5918e2a0bf27d44a0cd2ded4bc9f3e0a13b11c
vundi <vundi4773@gmail.com>
2024-09-05 18:05:08 +0300

    chatterbox

--- server/app.py
+++ server/app.py
@@ -14,13 +14,53 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET','POST'])
 def messages():
-    return ''
+    if request.method =='GET':
+        messages =Message.query.order_by('created_at').all()
+        reponse =make_response(jsonify([message.to_dict()for message in messages]),
+                               200)
+            
+        return reponse
+    
+    elif request.method =='POST':
+        data =request.get_json()
+        message =Message(
+            body = data['body'],
+            username =data['username']
+        )
+        db.session.add(message)
+        db.session.commit()
+        message_dict =message.to_dict()
 
-@app.route('/messages/<int:id>')
+
+    return make_response(jsonify(message_dict),201)
+
+@app.route('/messages/<int:id>', methods =['PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    message =Message.query.filter_by(id=id).first()
+
+    if request.method == 'PATCH':
+        data = request.get_json()
+
+        if "body" in data:
+            message.body =data['body']
+            
+
+        db.session.add(message)
+        db.session.commit()
+        message_dict =message.to_dict()
+        return make_response(jsonify(message_dict))
+    
+    elif request.method == 'DELETE':  
+        db.session.delete(message)
+        db.session.commit()
+        response_body ={
+            "delete successful":True,
+            "message":"Message deleted"
+        }  
+        return make_response(response_body)
+
 
 if __name__ == '__main__':
     app.run(port=5555)
--- server/models.py
+++ server/models.py
@@ -10,5 +10,10 @@ db = SQLAlchemy(metadata=metadata)
 
 class Message(db.Model, SerializerMixin):
     __tablename__ = 'messages'
-
     id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String)
+    username = db.Column(db.String)
+    created_at =db.Column(db.DateTime, server_default=db.func.now())
+    updated_at =db.Column(db.DateTime, onupdate =db.func.now())
+
+    
